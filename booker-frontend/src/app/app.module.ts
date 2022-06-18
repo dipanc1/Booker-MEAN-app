92f6af17ec20ec81a9629615904d8b6336f674dc
dipanc1 <dipanchhabra@gmail.com>
2022-06-18 15:45:24 +0530

    login, sign up in angular

---
+++ booker-frontend/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderModule } from './header/header.module';
+import { LoginModule } from './auth/login/login.module';
+import { SignUpModule } from './auth/sign-up/sign-up.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HeaderModule,
+    LoginModule,
+    SignUpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
